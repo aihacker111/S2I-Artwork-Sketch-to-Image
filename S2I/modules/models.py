a627f7765e501e56f1a720fce82f6ea9296d142c
Theodore <tinprocoder0908@gmail.com>
2024-06-07 18:28:32 +0700

    fixing logic

--- S2I/modules/models.py
+++ S2I/modules/models.py
@@ -23,6 +23,7 @@ class RelationShipConvolution(torch.nn.Module):
 class PrimaryModel:
     def __init__(self, backbone_diffusion_path='stabilityai/sd-turbo'):
         self.backbone_diffusion_path = backbone_diffusion_path
+        self.p_ckpt = None
         self.global_unet = None
         self.global_vae = None
         self.global_tokenizer = None
@@ -33,15 +34,12 @@ class PrimaryModel:
     def _load_model(path, model_class, unet_mode=False):
         if unet_mode:
             model = model_class.from_pretrained(path, subfolder='unet')
-            model = copy.deepcopy(model)
             model = model.to('cuda')
             return model
         else:
             model = model_class.from_pretrained(path, subfolder='vae')
-            model = copy.deepcopy(model)
             model = model.to('cuda')
             return model
-
 
     def one_step_scheduler(self):
         noise_scheduler_1step = DDPMScheduler.from_pretrained(self.backbone_diffusion_path, subfolder="scheduler")
@@ -59,39 +57,50 @@ class PrimaryModel:
         vae.decoder.ignore_skip = False
         return vae
 
-    def from_pretrained(self, model_name):
-        if self.global_tokenizer is None:
-            self.global_tokenizer = AutoTokenizer.from_pretrained(self.backbone_diffusion_path, subfolder="tokenizer")
+    def from_pretrained(self, model_name, load_model=False):
+        if load_model and model_name is None:
+            if self.global_tokenizer is None:
+                self.global_tokenizer = AutoTokenizer.from_pretrained(self.backbone_diffusion_path, subfolder="tokenizer")
 
-        if self.global_text_encoder is None:
-            self.global_text_encoder = CLIPTextModel.from_pretrained(self.backbone_diffusion_path, subfolder="text_encoder").to(device='cuda')
+            if self.global_text_encoder is None:
+                self.global_text_encoder = CLIPTextModel.from_pretrained(self.backbone_diffusion_path,
+                                                                         subfolder="text_encoder").to(device='cuda')
 
-        if self.global_scheduler is None:
-            self.global_scheduler = self.one_step_scheduler()
+            if self.global_scheduler is None:
+                self.global_scheduler = self.one_step_scheduler()
 
-        if self.global_vae is None:
-            self.global_vae = self._load_model(self.backbone_diffusion_path, AutoencoderKL)
-            self.global_vae = self.skip_connections(self.global_vae)
+            if self.global_vae is None:
+                self.global_vae = self._load_model(self.backbone_diffusion_path, AutoencoderKL)
+                self.global_vae = self.skip_connections(self.global_vae)
 
-        if self.global_unet is None:
-            self.global_unet = self._load_model(self.backbone_diffusion_path, UNet2DConditionModel, unet_mode=True)
-            p_ckpt_path = download_models()
-            p_ckpt = get_model_path(model_name=model_name, model_paths=p_ckpt_path)
-            sd = torch.load(p_ckpt, map_location="cpu")
-            conv_in_pretrained = copy.deepcopy(self.global_unet.conv_in)
-            self.global_unet.conv_in = RelationShipConvolution(conv_in_pretrained, self.global_unet.conv_in)
-            unet_lora_config = LoraConfig(r=sd["rank_unet"], init_lora_weights="gaussian", target_modules=sd["unet_lora_target_modules"])
-            vae_lora_config = LoraConfig(r=sd["rank_vae"], init_lora_weights="gaussian", target_modules=sd["vae_lora_target_modules"])
-            self.global_vae.add_adapter(vae_lora_config, adapter_name="vae_skip")
-            _sd_vae = self.global_vae.state_dict()
-            for k in sd["state_dict_vae"]:
-                _sd_vae[k] = sd["state_dict_vae"][k]
-            self.global_vae.load_state_dict(_sd_vae)
-            self.global_unet.add_adapter(unet_lora_config)
-            _sd_unet = self.global_unet.state_dict()
-            for k in sd["state_dict_unet"]:
-                _sd_unet[k] = sd["state_dict_unet"][k]
-            self.global_unet.load_state_dict(_sd_unet, strict=False)
+            if self.global_unet is None and self.p_ckpt is None:
+                self.global_unet = self._load_model(self.backbone_diffusion_path, UNet2DConditionModel, unet_mode=True)
+                p_ckpt_path = download_models()
+                self.p_ckpt = get_model_path(model_name=model_name, model_paths=p_ckpt_path)
+        else:
+            self.tokenizer = self.global_tokenizer
+            self.text_encoder = self.global_text_encoder
+            self.unet = self.global_unet
+            self.vae = self.global_vae
+            self.scheduler = self.global_scheduler
+            if model_name is not None:
+                sd = torch.load(self.p_ckpt, map_location="cpu")
+                conv_in_pretrained = copy.deepcopy(self.global_unet.conv_in)
+                self.global_unet.conv_in = RelationShipConvolution(conv_in_pretrained, self.global_unet.conv_in)
+                unet_lora_config = LoraConfig(r=sd["rank_unet"], init_lora_weights="gaussian",
+                                              target_modules=sd["unet_lora_target_modules"])
+                vae_lora_config = LoraConfig(r=sd["rank_vae"], init_lora_weights="gaussian",
+                                             target_modules=sd["vae_lora_target_modules"])
+                self.global_vae.add_adapter(vae_lora_config, adapter_name="vae_skip")
+                _sd_vae = self.global_vae.state_dict()
+                for k in sd["state_dict_vae"]:
+                    _sd_vae[k] = sd["state_dict_vae"][k]
+                self.global_vae.load_state_dict(_sd_vae)
+                self.global_unet.add_adapter(unet_lora_config)
+                _sd_unet = self.global_unet.state_dict()
+                for k in sd["state_dict_unet"]:
+                    _sd_unet[k] = sd["state_dict_unet"][k]
+                self.global_unet.load_state_dict(_sd_unet, strict=False)
+            self.unet.to('cuda')
+            self.text_encoder.to('cuda')
 
-        self.global_text_encoder.to('cuda')
-        self.global_unet.to('cuda')
